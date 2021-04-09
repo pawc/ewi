d35d131b4d6d4592291ab6f84cbef68d3d8a3448
pawc <pswid89@gmail.com>
2021-04-09 22:27:38 +0200

    poprawka

--- src/main/java/pl/pawc/ewi/repository/RaportRepository.java
+++ src/main/java/pl/pawc/ewi/repository/RaportRepository.java
@@ -13,10 +13,10 @@ public interface RaportRepository extends CrudRepository<Raport, String> {
     @Query(value = "select concat(m.nazwa, ' (', m.id, ')') as maszyna, n.jednostka, "+
         "round(sum(z.wartosc*n.wartosc), 2) as suma, concat(m.nazwa, n.jednostka) as id "+
         "from Maszyna m " +
-        "join Dokument d on m.id = d.maszyna_id " +
+        "join Dokument d on year(d.data) = ?1 and month(d.data) = ?2 " +
+        "and m.id = d.maszyna_id " +
         "join Zuzycie z on d.numer = z.dokument_numer " +
         "join Norma n on z.norma_id = n.id " +
-        "where year(d.data) = ?1 and month(d.data) = ?2 " +
         "group by m.nazwa, n.jednostka",
         nativeQuery = true
     )
