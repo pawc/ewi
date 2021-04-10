0e46f70335a099e090483808b278425580500248
pawc <pswid89@gmail.com>
2021-04-10 23:37:11 +0200

    mariadb fix

--- src/main/java/pl/pawc/ewi/repository/RaportRepository.java
+++ src/main/java/pl/pawc/ewi/repository/RaportRepository.java
@@ -12,11 +12,11 @@ public interface RaportRepository extends CrudRepository<Raport, String> {
 
     @Query(value = "select concat(m.nazwa, ' (', m.id, ')') as maszyna, n.jednostka, "+
         "round(sum(z.wartosc*n.wartosc), 2) as suma, concat(m.nazwa, n.jednostka) as id "+
-        "from Maszyna m " +
-        "join Dokument d on year(d.data) = ?1 and month(d.data) = ?2 " +
+        "from maszyna m " +
+        "join dokument d on year(d.data) = ?1 and month(d.data) = ?2 " +
         "and m.id = d.maszyna_id " +
-        "join Zuzycie z on d.numer = z.dokument_numer " +
-        "join Norma n on z.norma_id = n.id " +
+        "join zuzycie z on d.numer = z.dokument_numer " +
+        "join norma n on z.norma_id = n.id " +
         "group by m.nazwa, n.jednostka",
         nativeQuery = true
     )
