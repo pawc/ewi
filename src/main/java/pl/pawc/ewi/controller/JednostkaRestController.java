2569607b3d7943a0a45d36f8dd2c5f27e8309b84
pawc <pswid89@gmail.com>
2022-10-29 22:47:56 +0200

    jednostka - 1

---
+++ src/main/java/pl/pawc/ewi/controller/JednostkaRestController.java
@@ -0,0 +1,47 @@
+package pl.pawc.ewi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.pawc.ewi.entity.Jednostka;
+import pl.pawc.ewi.repository.JednostkaRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class JednostkaRestController {
+
+    private static final Logger logger = LogManager.getLogger(JednostkaRestController.class);
+    private final JednostkaRepository jednostkaRepository;
+
+    @PostMapping("jednostka")
+    public void jednostka(
+            @RequestBody Jednostka jednostka,
+            HttpServletRequest request) {
+
+        Jednostka j = jednostkaRepository.findById(jednostka.getId()).orElse(null);
+        if(j != null) j.setNazwa(jednostka.getNazwa());
+        else j = new Jednostka(jednostka.getNazwa());
+
+        jednostkaRepository.save(j);
+
+        String ip = request.getHeader("X-Real-IP") != null ? request.getHeader("X-Real-IP") : request.getRemoteAddr();
+        logger.info("[{}] POST /jednostka {}", ip, j);
+    }
+
+    @GetMapping("jednostki")
+    public List<Jednostka> jednostki(HttpServletRequest request){
+
+        String ip = request.getHeader("X-Real-IP") != null ? request.getHeader("X-Real-IP") : request.getRemoteAddr();
+        logger.info("[{}] /jednostki", ip);
+        return jednostkaRepository.findAll();
+
+    }
+
+}
---
+++ src/main/java/pl/pawc/ewi/entity/Jednostka.java
@@ -0,0 +1,42 @@
+package pl.pawc.ewi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Jednostka {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "nazwa")
+    private String nazwa;
+
+    public Jednostka(){
+    }
+
+    public Jednostka(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    @Override
+    public String toString() {
+        return "Jednostka{" +
+                "id=" + id +
+                ", nazwa='" + nazwa + '\'' +
+                '}';
+    }
+
+}
