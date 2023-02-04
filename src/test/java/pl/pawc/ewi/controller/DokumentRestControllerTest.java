1e885455f4205028c94efe6f63007453228f08a6
pawc <pswid89@gmail.com>
2023-02-04 10:12:36 +0100

    DokumentRestControllerTest - 2

--- src/test/java/pl/pawc/ewi/controller/DokumentRestControllerTest.java
+++ src/test/java/pl/pawc/ewi/controller/DokumentRestControllerTest.java
@@ -1,96 +1,1 @@
-package pl.pawc.ewi.controller;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import pl.pawc.ewi.entity.Dokument;
-
-import javax.servlet.ServletContext;
-
-import java.math.BigDecimal;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest(properties = {
-    "spring.datasource.driverClassName=org.h2.Driver",
-    "spring.datasource.url=jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1",
-})
-class DokumentRestControllerTest {
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    private MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @BeforeEach
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-        ServletContext servletContext = webApplicationContext.getServletContext();
-
-        assertNotNull(servletContext);
-        assertNotNull(webApplicationContext.getBean("dokumentRestController"));
-    }
-
-    @Test
-    void dokumentGet() throws Exception {
-
-        mockMvc.perform(get("/dokument")
-            .param("numer", "1/04/2022/C1"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(result -> {
-                Dokument dokument = objectMapper.readValue(result.getResponse().getContentAsString(), Dokument.class);
-                assertNotNull(dokument);
-                assertEquals("1/04/2022/C1", dokument.getNumer());
-                assertNotNull(dokument.getMaszyna());
-                assertEquals("C1", dokument.getMaszyna().getId());
-                assertEquals(new BigDecimal("10.0"), dokument.getKilometry());
-                assertEquals(new BigDecimal("10.0"), dokument.getKilometryPrzyczepa());
-            });
-
-        mockMvc.perform(get("/dokument")
-                    .param("numer", "99/04/2022/C1"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(result -> {
-                Dokument dokument = objectMapper.readValue(result.getResponse().getContentAsString(), Dokument.class);
-                assertNotNull(dokument);
-                assertNull(dokument.getNumer());
-                assertNull(dokument.getMaszyna());
-                assertNull(dokument.getMaszyna());
-                assertNull(dokument.getKilometry());
-                assertNull(dokument.getKilometryPrzyczepa());
-            });
-
-    }
-
-    @Test
-    void dokumentyGet() throws Exception {
-
-        mockMvc.perform(get("/dokumentyGet")
-                .param("rok", "2022")
-                .param("miesiac", "4")
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(result -> {
-                System.out.println(result);
-                List<Dokument> dokumenty = objectMapper.readValue(result.getResponse().getContentAsString(), List.class);
-                assertEquals(17, dokumenty.size());
-            });
-
-    }
-
-}
+package pl.pawc.ewi.controller;import com.fasterxml.jackson.databind.ObjectMapper;import com.fasterxml.jackson.databind.ObjectWriter;import com.fasterxml.jackson.databind.SerializationFeature;import org.junit.jupiter.api.BeforeEach;import org.junit.jupiter.api.MethodOrderer;import org.junit.jupiter.api.Order;import org.junit.jupiter.api.Test;import org.junit.jupiter.api.TestMethodOrder;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.boot.test.context.SpringBootTest;import org.springframework.test.web.servlet.MockMvc;import org.springframework.test.web.servlet.setup.MockMvcBuilders;import org.springframework.web.context.WebApplicationContext;import pl.pawc.ewi.entity.Dokument;import pl.pawc.ewi.entity.Maszyna;import pl.pawc.ewi.entity.Norma;import pl.pawc.ewi.entity.Zuzycie;import javax.servlet.ServletContext;import java.math.BigDecimal;import java.sql.Date;import java.util.Arrays;import java.util.Collections;import java.util.List;import static org.junit.jupiter.api.Assertions.assertEquals;import static org.junit.jupiter.api.Assertions.assertNotNull;import static org.junit.jupiter.api.Assertions.assertNull;import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;@SpringBootTest(properties = {    "spring.datasource.driverClassName=org.h2.Driver",    "spring.datasource.url=jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1",})@TestMethodOrder(MethodOrderer.OrderAnnotation.class)class DokumentRestControllerTest {    @Autowired    private WebApplicationContext webApplicationContext;    private MockMvc mockMvc;    @Autowired    ObjectMapper objectMapper;    @BeforeEach    public void setup() {        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();        ServletContext servletContext = webApplicationContext.getServletContext();        assertNotNull(servletContext);        assertNotNull(webApplicationContext.getBean("dokumentRestController"));    }    @Test    @Order(1)    void dokumentGetTest() throws Exception {        mockMvc.perform(get("/dokument")            .param("numer", "1/04/2022/C1"))            .andExpect(status().isOk())            .andExpect(content().contentType("application/json"))            .andExpect(result -> {                Dokument dokument = objectMapper.readValue(result.getResponse().getContentAsString(), Dokument.class);                assertNotNull(dokument);                assertEquals("1/04/2022/C1", dokument.getNumer());                assertNotNull(dokument.getMaszyna());                assertEquals("C1", dokument.getMaszyna().getId());                assertEquals(new BigDecimal("10.0"), dokument.getKilometry());                assertEquals(new BigDecimal("10.0"), dokument.getKilometryPrzyczepa());            });        mockMvc.perform(get("/dokument")                    .param("numer", "99/04/2022/C1"))            .andExpect(status().isOk())            .andExpect(content().contentType("application/json"))            .andExpect(result -> {                Dokument dokument = objectMapper.readValue(result.getResponse().getContentAsString(), Dokument.class);                assertNotNull(dokument);                assertNull(dokument.getNumer());                assertNull(dokument.getMaszyna());                assertNull(dokument.getMaszyna());                assertNull(dokument.getKilometry());                assertNull(dokument.getKilometryPrzyczepa());            });    }    @Test    @Order(2)    void dokumentyGetTest() throws Exception {        mockMvc.perform(get("/dokumentyGet")                .param("rok", "2022")                .param("miesiac", "4")            )            .andExpect(status().isOk())            .andExpect(content().contentType("application/json"))            .andExpect(result -> {                System.out.println(result);                List<Dokument> dokumenty = objectMapper.readValue(result.getResponse().getContentAsString(), List.class);                assertEquals(17, dokumenty.size());            });    }    @Test    @Order(3)    void dokumentPostTest() throws Exception {        Maszyna maszyna = new Maszyna();        maszyna.setId("C1");        Zuzycie zuzycie = new Zuzycie();        zuzycie.setId(123);        zuzycie.setWartosc(BigDecimal.ZERO);        zuzycie.setOgrzewanie(BigDecimal.ZERO);        zuzycie.setZatankowano(BigDecimal.ZERO);        Norma norma = new Norma();        norma.setId(1);        zuzycie.setNorma(norma);        Dokument dokument = new Dokument();        dokument.setNumer("80/09/2022/C1");        dokument.setKilometry(BigDecimal.ZERO);        dokument.setKilometryPrzyczepa(BigDecimal.ZERO);        dokument.setData(new Date(System.currentTimeMillis()));        dokument.setMaszyna(maszyna);        dokument.setZuzycie(Arrays.asList(zuzycie));        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);        ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();        String requestJson = ow.writeValueAsString(dokument);        mockMvc.perform(post("/dokument")                .contentType(APPLICATION_JSON_UTF8)                .content(requestJson))            .andExpect(status().isOk());    }    @Test    @Order(4)    void dokumentPutTest() throws Exception {        Maszyna maszyna = new Maszyna();        maszyna.setId("C1");        Zuzycie zuzycie = new Zuzycie();        zuzycie.setId(123);        zuzycie.setWartosc(BigDecimal.valueOf(13.34));        zuzycie.setOgrzewanie(BigDecimal.valueOf(57.78));        zuzycie.setZatankowano(BigDecimal.valueOf(99.98));        Norma norma = new Norma();        norma.setId(1);        zuzycie.setNorma(norma);        Dokument dokument = new Dokument();        dokument.setKilometry(BigDecimal.valueOf(10.0));        dokument.setKilometryPrzyczepa(BigDecimal.valueOf(10.0));        dokument.setData(new Date(System.currentTimeMillis()));        dokument.setMaszyna(maszyna);        dokument.setZuzycie(Collections.EMPTY_LIST);        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);        ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();        dokument.setNumer("81/09/2022/C1");        String requestJson = ow.writeValueAsString(dokument);        mockMvc.perform(put("/dokument")                        .contentType(APPLICATION_JSON_UTF8)                        .content(requestJson))                .andExpect(status().isBadRequest());        dokument.setNumer("80/09/2022/C1");        requestJson = ow.writeValueAsString(dokument);        mockMvc.perform(put("/dokument")                    .contentType(APPLICATION_JSON_UTF8)                    .content(requestJson))            .andExpect(status().isOk());    }    @Test    @Order(5)    void dokumentDeleteTest() throws Exception {        mockMvc.perform(delete("/dokument")                        .param("numer", "80/04/2022/C1"))                .andExpect(status().isBadRequest());        mockMvc.perform(delete("/dokument")                        .param("numer", "80/09/2022/C1"))                .andExpect(status().isOk());    }}
--- src/test/java/pl/pawc/ewi/service/DokumentServiceTest.java
+++ src/test/java/pl/pawc/ewi/service/DokumentServiceTest.java
@@ -2,6 +2,7 @@ package pl.pawc.ewi.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +16,6 @@ import javax.transaction.Transactional;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = {
 		"spring.datasource.driverClassName=org.h2.Driver",
@@ -40,11 +39,11 @@ class DokumentServiceTest {
 		for (Kilometry kilometry : kilometryRepository.findAll()) {
 			Double expected = dokumentRepository.getSumaKilometry(kilometry.getMaszyna().getId(), kilometry.getRok(), kilometry.getMiesiac());
 			BigDecimal actual = dokumentService.getSumaKilometry(kilometry.getMaszyna().getId(), kilometry.getRok(), kilometry.getMiesiac(), null);
-			assertEquals(BigDecimal.valueOf(expected), actual);
+			Assertions.assertEquals(BigDecimal.valueOf(expected), actual);
 
 		}
 
-		assertEquals(BigDecimal.ZERO, dokumentService.getSumaKilometry("C11", 2022, 4, null));
+		Assertions.assertEquals(BigDecimal.ZERO, dokumentService.getSumaKilometry("C11", 2022, 4, null));
 
 	}
 
@@ -53,21 +52,21 @@ class DokumentServiceTest {
 	void testGetSumaKilometryBeforeDate() {
 
 		try {
-			assertEquals(new BigDecimal("120.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"1/04/2022/C1"));
-			assertEquals(new BigDecimal("130.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"2/04/2022/C1"));
-			assertEquals(new BigDecimal("141.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"3/04/2022/C1"));
-			assertEquals(new BigDecimal("141.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"4/04/2022/C1"));
-			assertEquals(new BigDecimal("146.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"5/04/2022/C1"));
-			assertEquals(new BigDecimal("147.4"), dokumentService.getSumaKilometry("C1", 2022, 4,"6/04/2022/C1"));
-			assertEquals(new BigDecimal("154.4"), dokumentService.getSumaKilometry("C1", 2022, 4,"7/04/2022/C1"));
-			assertEquals(new BigDecimal("172.9"), dokumentService.getSumaKilometry("C1", 2022, 4,"8/04/2022/C1"));
-			assertEquals(new BigDecimal("190.9"), dokumentService.getSumaKilometry("C1", 2022, 4,"9/04/2022/C1"));
-			assertEquals(new BigDecimal("209.9"), dokumentService.getSumaKilometry("C1", 2022, 4,"10/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("120.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"1/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("130.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"2/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("141.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"3/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("141.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"4/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("146.0"), dokumentService.getSumaKilometry("C1", 2022, 4,"5/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("147.4"), dokumentService.getSumaKilometry("C1", 2022, 4,"6/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("154.4"), dokumentService.getSumaKilometry("C1", 2022, 4,"7/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("172.9"), dokumentService.getSumaKilometry("C1", 2022, 4,"8/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("190.9"), dokumentService.getSumaKilometry("C1", 2022, 4,"9/04/2022/C1"));
+			Assertions.assertEquals(new BigDecimal("209.9"), dokumentService.getSumaKilometry("C1", 2022, 4,"10/04/2022/C1"));
 		} catch (DocumentNotFoundException e) {
-			fail();
+			Assertions.fail();
 		}
 
-		assertThrows(DocumentNotFoundException.class,
+		Assertions.assertThrows(DocumentNotFoundException.class,
 				() -> dokumentService.getSumaKilometry("C1", 2022, 4,"11/04/2022/C1"));
 
 	}
@@ -83,29 +82,17 @@ class DokumentServiceTest {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(dokument1.getData());
 
-		assertNotNull(dokument1);
-		assertEquals(new BigDecimal("25"), dokument1.getKilometry());
-		assertEquals(new BigDecimal("25"), dokument1.getKilometryPrzyczepa());
-		assertEquals(2, dokument1.getZuzycie().size());
-		assertEquals(17, cal.get(Calendar.DAY_OF_MONTH));
-		assertEquals(3, cal.get(Calendar.MONTH));
-		assertEquals(2022, cal.get(Calendar.YEAR));
-
-		input = "{\"numer\":\"15/04/2022/C1\",\"data\":\"2023-05-29\",\"kilometry\":\"9\",\"kilometryPrzyczepa\":\"9\",\"maszyna\":{\"id\":\"C1\"},\"zuzycie\":[{\"wartosc\":\"9\",\"id\":\"1\",\"norma\":{\"id\":\"1\",\"wartosc\":\"1\"},\"zatankowano\":\"9\",\"ogrzewanie\":\"9\"},{\"wartosc\":\"9\",\"id\":\"2\",\"norma\":{\"id\":\"2\",\"wartosc\":\"2\"},\"zatankowano\":\"9\",\"ogrzewanie\":\"0\"}]}";
-		dokument = objectMapper.readValue(input, Dokument.class);
-		dokumentService.put(dokument);
-		dokument1 = dokumentService.get("15/04/2022/C1");
-		cal.setTime(dokument1.getData());
-
-		assertEquals(new BigDecimal("9"), dokument1.getKilometry());
-		assertEquals(new BigDecimal("9"), dokument1.getKilometryPrzyczepa());
-		assertEquals(29, cal.get(Calendar.DAY_OF_MONTH));
-		assertEquals(4, cal.get(Calendar.MONTH));
-		assertEquals(2023, cal.get(Calendar.YEAR));
+		Assertions.assertNotNull(dokument1);
+		Assertions.assertEquals(new BigDecimal("25"), dokument1.getKilometry());
+		Assertions.assertEquals(new BigDecimal("25"), dokument1.getKilometryPrzyczepa());
+		Assertions.assertEquals(2, dokument1.getZuzycie().size());
+		Assertions.assertEquals(17, cal.get(Calendar.DAY_OF_MONTH));
+		Assertions.assertEquals(3, cal.get(Calendar.MONTH));
+		Assertions.assertEquals(2022, cal.get(Calendar.YEAR));
 
 		dokumentService.delete("15/04/2022/C1");
 
-		assertThrows(DocumentNotFoundException.class, () -> dokumentService.delete("15/04/2022/C1"));
+		Assertions.assertThrows(DocumentNotFoundException.class, () -> dokumentService.delete("15/04/2022/C1"));
 
 	}
 
