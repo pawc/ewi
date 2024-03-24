04d9bea8d413ac265047e36fece5c55537146479
pawc <pswid89@proton.me>
2024-03-24 15:40:54 +0100

    profiles, sb 3.2.4

--- src/main/java/pl/pawc/ewi/config/EwiDevConfig.java
+++ src/main/java/pl/pawc/ewi/config/EwiDevConfig.java
@@ -4,27 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@Profile("prod")
-public class EwiConfig {
+@Profile("dev")
+public class EwiDevConfig {
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/home")
-                    .permitAll()
-                    .requestMatchers("/css/**")
-                    .permitAll()
-                    .anyRequest().authenticated()
-            )
-            .oauth2Login(login ->
-                    login.loginPage("/home")
+                .anyRequest()
+                .permitAll()
             )
             .build();
     }
